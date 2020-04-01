5530475d13daf6126fbd4496cae2e22aedb82b4e
Nikolay Yarlychenko <yarlychenkonikolay@gmail.com>
2020-04-01 20:13:40 +0300

    Initial Commit

---
+++ GameNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameNews
+//
+//  Created by Nikolay Yarlychenko on 01.04.2020.
+//  Copyright © 2020 Nikolay Yarlychenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
