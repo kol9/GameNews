eedf2f26200a74c00e7a2bec9b10f0d94d91b89d
Nikolay Yarlychenko <yarlychenkonikolay@gmail.com>
2020-04-03 23:03:44 +0300

    Done task

---
+++ GameNews/GameInfo.swift
@@ -0,0 +1,9 @@
+//
+//  GameInfo.swift
+//  GameNews
+//
+//  Created by Nikolay Yarlychenko on 02.04.2020.
+//  Copyright © 2020 Nikolay Yarlychenko. All rights reserved.
+//
+
+import Foundation
---
+++ GameNews/SettingsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsViewController.swift
+//  GameNews
+//
+//  Created by Nikolay Yarlychenko on 03.04.2020.
+//  Copyright © 2020 Nikolay Yarlychenko. All rights reserved.
+//
+
+import Foundation
